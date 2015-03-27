3ea9638263643c9d8b5e9a48f9de8675296a3f75
Sam Lau <liuyaozhu13hao@163.com>
2015-03-27 15:42:22 +0800

    finish design display stories ui in storyboard

---
+++ DesignerNewsForObjc/Sections/Story/StoryViewController.h
@@ -0,0 +1,14 @@
+//
+//  StoryViewController.h
+//  DesignerNewsForObjc
+//
+//  Created by Sam Lau on 3/27/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryViewController : UITableViewController
+
+
+@end
---
+++ DesignerNewsForObjc/Sections/Story/StoryViewController.m
@@ -0,0 +1,19 @@
+//
+//  StoryViewController.m
+//  DesignerNewsForObjc
+//
+//  Created by Sam Lau on 3/27/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import "StoryViewController.h"
+
+@implementation StoryViewController
+
+#pragma mark - View controller lifecycle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
