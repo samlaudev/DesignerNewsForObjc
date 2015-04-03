e1013d4eb35d58161c1ebfd44bbff6ccb3c036a4
Sam Lau <liuyaozhu13hao@163.com>
2015-04-03 14:47:08 +0800

    add login mock service

--- Pods/Target Support Files/Pods-DesignerNewsForObjcTests-ReactiveViewModel/Pods-DesignerNewsForObjcTests-ReactiveViewModel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DesignerNewsForObjcTests_ReactiveViewModel : NSObject
-@end
-@implementation PodsDummy_Pods_DesignerNewsForObjcTests_ReactiveViewModel
-@end
