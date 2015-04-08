b70dfb6ec8eeb52a128b2778b9469eb598ce53da
Sam Lau <liuyaozhu13hao@163.com>
2015-04-08 19:28:59 +0800

    Create WebViewController

---
+++ DesignerNewsForObjc/Sections/Web/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  DesignerNewsForObjc
+//
+//  Created by Sam Lau on 4/8/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ DesignerNewsForObjc/Sections/Web/WebViewController.m
@@ -0,0 +1,22 @@
+//
+//  WebViewController.m
+//  DesignerNewsForObjc
+//
+//  Created by Sam Lau on 4/8/15.
+//  Copyright (c) 2015 Sam Lau. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+@end
